fd061160e5cde57aa382bb6b9c19f42cff12f0f9
DG <debopamgupta3000@gmail.com>
2020-11-12 14:05:28 +0530

    Change prefix to $

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const { Client } = require("discord.js");
 const client = new Client();
 
-const prefix = `h+`;
+const prefix = `$`;
 client.once("ready", () => {
   console.log("BEEP BOOP ! Ready!");
-  client.user.setActivity("h+help", { type: "LISTENING" });
+  client.user.setActivity("$help", { type: "LISTENING" });
 });
 
 client.login(process.env.BOT_TOKEN);
@@ -14,14 +14,14 @@ client.on("message", async (msg) => {
   // TODO: BRING IN A COMMAND HANDLER TO HANDLE THE COMMANDS
   msg.content = msg.content.toLowerCase();
   if (!msg.content.startsWith(prefix) || msg.author.bot) return;
-  if (msg.content == "h+ping") {
+  if (msg.content == "$ping") {
     await msg.react("🏓");
     await msg.channel.send("PONG! 🏓");
   }
-  if (msg.content == "h+help") {
+  if (msg.content == "$help") {
     await msg.react("💡");
     await msg.channel.send(
-      "***Hey this bot is currently in development, current commands available are -*** \n `h+help` and `h+ping`"
+      "***Hey this bot is currently in development, current commands available are -*** \n `$help` and `$ping`"
     );
   }
 });
